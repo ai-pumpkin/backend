b8208b5e0c2fc9b5fa10bc4d83331f40f3285f23
Ghita Houir Alami <ghitaha@Ghitas-MacBook-Pro.local>
2023-10-27 23:29:55 -0700

    oac update + text2speech

--- oac.py
+++ oac.py
@@ -1,8 +1,8 @@
-
 from gradio_client import Client
 import yaml
 
 import openai
+
 
 class OpenAIClient:
     def __init__(self, api_key, **kwargs):
@@ -14,7 +14,7 @@ class OpenAIClient:
         """
         self.api_key = api_key
         self.params = kwargs
-        openai.api_key = self.api_key      
+        openai.api_key = self.api_key
 
     def chat_completion(self, prompt):
         """
@@ -26,23 +26,24 @@ class OpenAIClient:
         message = {"role": "system", "content": prompt}
         try:
             response = openai.ChatCompletion.create(
-                model=self.params.get('model', 'gpt-3.5-turbo'),
+                model=self.params.get("model", "gpt-3.5-turbo"),
                 messages=[message],
             )
-            return response['choices'][0]['message']['content']
+            return response["choices"][0]["message"]["content"]
         except Exception as e:
             print(f"An error occurred: {e}")
             return None
 
 
-with open('config.yaml', 'r') as f:
+with open("config/config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
 img_client = Client("https://xinyu1205-recognize-anything.hf.space/")
 oac = OpenAIClient(
-    api_key=config['open_api_key'],
+    api_key=config["openai"],
     model="gpt-3.5-turbo",
 )
+
 
 class MyOAC:
     def __init__(self):
@@ -51,5 +52,3 @@ class MyOAC:
     def __call__(self, tags):
         prompt = f"You are a scary pumpkin during Halloween. There is a person in front of you that you are trying to scare. I took a picture of this person and I am going to describe it to you using tags. Delimited by  {tags}. Write a creepy sentence about this person using details about the person and their surroundings."
         return oac.chat_completion(prompt)
-
-
--- text2speech.py
+++ text2speech.py
@@ -1,4 +1,4 @@
-from openaiclient import OpenAIClient
+from oac import OpenAIClient
 from elevenlabs import clone, generate, play
 from elevenlabs import set_api_key
 
@@ -16,6 +16,11 @@ class SaySomething(object):
             model="gpt-3.5-turbo",
         )
         set_api_key(eleven_key)
+        self.voice = voice = clone(
+            name="Creepy",
+            description="Scary voice. Very creepy and disturbing. Sometimes becomes very deep and demonic.",
+            files=["demon.mp3"],
+        )
 
     def write(self, image_tags):
         prompt = f"You are a scary pumpkin during Halloween. There is a person in front of you that you are trying to scare. I took a picture of this person and I am going to describe it to you using tags. Delimited by  {image_tags}. Write a creepy sentence about this person using details about the person and their surroundings."
@@ -23,14 +28,9 @@ class SaySomething(object):
 
     def talk(self, image_tags):
         # response = self.write(image_tags)
-        voice = clone(
-            name="Creepy",
-            description="Scary voice. Very creepy and disturbing. Sometimes becomes very deep and demonic.",
-            files=["demon.mp3"],
-        )
         audio = generate(
             text="Hello! Are you scared yet? I'm behind you... Hahahahaha. Look at the mirror... I'm right here... Right behind you... Can you see me?",
-            voice=voice,
+            voice=self.voice,
             model="eleven_multilingual_v2",
         )
 
