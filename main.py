c9779020ee03b352b7cb652788a432f60cd9f14f
Yves Martin <59341242+yvesmartindestaillades@users.noreply.github.com>
2023-10-27 19:11:34 -0700

    server run, update requirements

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 from fastapi import FastAPI
-from sqlalchemy.orm import Session
-
-from fastapi_utils.session import FastAPISessionMaker
-from fastapi_utils.tasks import repeat_every
-
-database_uri = f"sqlite:///./test.db?check_same_thread=False"
-sessionmaker = FastAPISessionMaker(database_uri)
+import asyncio
 
 app = FastAPI()
 
-
-def remove_expired_tokens(db: Session) -> None:
-    """Pretend this function deletes expired tokens from the database"""
-
+async def my_task():
+    while True:
+        # Replace this with your task logic
+        print("Running the task every second")
+        await asyncio.sleep(1)
 
 @app.on_event("startup")
-@repeat_every(seconds=60 * 60)  # 1 hour
-def remove_expired_tokens_task() -> None:
-    with sessionmaker.context_session() as db:
-        remove_expired_tokens(db=db)
-        
+async def startup_event():
+    asyncio.create_task(my_task())
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
